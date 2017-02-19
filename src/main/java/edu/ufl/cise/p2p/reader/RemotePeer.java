2cb0325fd52d2997592252a8be330435e34ce70f
adityaKant <adikant9@gmail.com>
2017-02-18 20:40:02 -0500

    "remote peer info class"

---
+++ src/main/java/edu/ufl/cise/p2p/reader/RemotePeer.java
@@ -0,0 +1,34 @@
+package edu.ufl.cise.p2p.reader;
+
+/**
+ * Created by aditya on 2/18/17.
+ */
+public class RemotePeer {
+    String ipAddress;
+    int port;
+    String peerId;
+    Boolean hasFile;
+
+    public RemotePeer(String ipAddress, int port, String peerId, Boolean hasFile){
+        this.ipAddress = ipAddress;
+        this.port = port;
+        this.peerId = peerId;
+        this.hasFile = hasFile;
+    }
+
+    public String getAddress(){
+        return ipAddress;
+    }
+
+    public int getPort(){
+        return port;
+    }
+
+    public String getPeerId(){
+        return peerId;
+    }
+
+    public Boolean hasFile(){
+        return hasFile;
+    }
+}
