cabc17b227eb9aac524dd426fb93f2bdbcf38cda
adityaKant <adikant9@gmail.com>
2017-04-08 18:02:04 -0400

    Peer handler added

---
+++ src/main/java/edu/ufl/cise/p2p/PeerHandler.java
@@ -0,0 +1,12 @@
+package edu.ufl.cise.p2p;
+
+import java.util.ArrayList;
+
+public class PeerHandler {
+    ArrayList<RemotePeer> remotePeers;
+    CommonPeerProperties commonProp;
+
+    public PeerHandler(ArrayList<RemotePeer> peers, CommonPeerProperties prop){
+        
+    }
+}
