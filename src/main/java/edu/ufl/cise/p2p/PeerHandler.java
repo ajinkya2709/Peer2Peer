c3585b651e22bc6a6f15b3ae6d71e2d575811167
adityaKant <adikant9@gmail.com>
2017-04-08 18:31:58 -0400

    changed Peer handler prop

--- src/main/java/edu/ufl/cise/p2p/PeerHandler.java
+++ src/main/java/edu/ufl/cise/p2p/PeerHandler.java
@@ -1,12 +1,21 @@
 package edu.ufl.cise.p2p;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class PeerHandler {
+public class PeerHandler implements Runnable{
     ArrayList<RemotePeer> remotePeers;
     CommonPeerProperties commonProp;
+    AtomicBoolean hasFile;
 
-    public PeerHandler(ArrayList<RemotePeer> peers, CommonPeerProperties prop){
-        
+    public PeerHandler(ArrayList<RemotePeer> peers, CommonPeerProperties prop, Boolean hasFile){
+        remotePeers = peers;
+        commonProp = prop;
+        this.hasFile.set(hasFile);
     }
+
+    public void run(){
+
+    }
+
 }
