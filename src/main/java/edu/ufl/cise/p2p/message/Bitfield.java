a4c1fad15e4b862d82258da0258720e6fb30d6ba
ajinkyagaikwad2709@gmail.com <ajinkyagaikwad2709@gmail.com>
2017-04-09 17:41:37 -0400

    Class Added to represent the Bitfield message

---
+++ src/main/java/edu/ufl/cise/p2p/message/Bitfield.java
@@ -0,0 +1,21 @@
+package edu.ufl.cise.p2p.message;
+
+import java.util.BitSet;
+
+public class Bitfield extends Message {
+
+	public Bitfield(byte[] payload) {
+		super(5, payload);
+	}
+
+	public Bitfield(BitSet bitSet) {
+		super(5, bitSet.toByteArray());
+	}
+
+	public BitSet getBitSet() {
+		if (payload != null) {
+			return BitSet.valueOf(payload);
+		}
+		return null;
+	}
+}
