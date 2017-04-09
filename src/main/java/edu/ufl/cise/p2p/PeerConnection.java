8fee142c21b9b458ef558f81f00a2925bd11c0ec
ajinkya2709 <ajinkyagaikwad2709@gmail.com>
2017-04-08 20:09:15 -0400

    Code added for handshaking

--- src/main/java/edu/ufl/cise/p2p/PeerConnection.java
+++ src/main/java/edu/ufl/cise/p2p/PeerConnection.java
@@ -1,18 +1,55 @@
 package edu.ufl.cise.p2p;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import edu.ufl.cise.p2p.message.Handshake;
+
 public class PeerConnection implements Runnable {
 
-	Integer localPeerId;
-	Integer remotePeerId;
+	Socket socket;
+	String localPeerId;
+	String remotePeerId;
+	boolean isClient;
+	ObjectInputStream inStream;
+	ObjectOutputStream outStream;
 
-	public PeerConnection(Integer localPeerId, Integer remotePeer) {
+	public PeerConnection(Socket socket, String localPeerId, String remotePeer,
+			boolean isClient) throws IOException {
+		this.socket = socket;
 		this.localPeerId = localPeerId;
 		this.remotePeerId = remotePeer;
+		this.isClient = isClient;
+		this.inStream = new ObjectInputStream(socket.getInputStream());
+		this.outStream = new ObjectOutputStream(socket.getOutputStream());
 	}
 
 	public void run() {
 		System.out.println("Connection created between :" + localPeerId
 				+ "\tand:" + remotePeerId);
+		try {
+
+			outStream.writeObject(new Handshake(Integer.parseInt(localPeerId)));
+
+			Handshake handShakeReceived = (Handshake) inStream.readObject();
+			System.out.println("Peer :[" + localPeerId
+					+ "] received Handshake from Peer :["
+					+ handShakeReceived.getPeerId() + "]");
+			//If it's a client, we can verify expected server
+
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
