8a3417193742c792fc497b5c353b49f2bf9e2806
ajinkyagaikwad2709@gmail.com <ajinkyagaikwad2709@gmail.com>
2017-04-10 00:47:19 -0400

    RemotePeer Parameter added in method createResponse

--- src/main/java/edu/ufl/cise/p2p/message/MessageProcessor.java
+++ src/main/java/edu/ufl/cise/p2p/message/MessageProcessor.java
@@ -3,10 +3,12 @@ package edu.ufl.cise.p2p.message;
 import java.util.BitSet;
 
 import edu.ufl.cise.p2p.FileHandler;
+import edu.ufl.cise.p2p.RemotePeer;
 
 public class MessageProcessor {
 
 	FileHandler fileHandler;
+	
 	
 	public MessageProcessor(FileHandler fileHandler){
 		this.fileHandler = fileHandler;
@@ -16,6 +18,10 @@ public class MessageProcessor {
 		BitSet bitSet = fileHandler.getBitSet();
 		if(bitSet.isEmpty()) return null;
 		return new Bitfield(bitSet);
+	}
+	
+	public Message createResponse(Message message, RemotePeer rPeer) {
+		return null;
 	}
 
 }
