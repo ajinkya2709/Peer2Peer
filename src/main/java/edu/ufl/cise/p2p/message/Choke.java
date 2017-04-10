99cf5e1893774596f388f3176e6d20f4afd897c1
ajinkyagaikwad2709@gmail.com <ajinkyagaikwad2709@gmail.com>
2017-04-10 01:22:32 -0400

    Added Choke and Unchoke

---
+++ src/main/java/edu/ufl/cise/p2p/message/Choke.java
@@ -0,0 +1,8 @@
+package edu.ufl.cise.p2p.message;
+
+public class Choke extends Message {
+
+	public Choke() {
+		super(0, null);
+	}
+}
---
+++ src/main/java/edu/ufl/cise/p2p/message/Unchoke.java
@@ -0,0 +1,8 @@
+package edu.ufl.cise.p2p.message;
+
+public class Unchoke extends Message {
+
+	public Unchoke() {
+		super(1, null);
+	}
+}
