cb391bf2672c4052d4bdf1a42f06cd382c8fce81
ajinkyagaikwad2709@gmail.com <ajinkyagaikwad2709@gmail.com>
2017-04-10 01:04:50 -0400

    Handled Bitfield Message

--- src/main/java/edu/ufl/cise/p2p/message/MessageProcessor.java
+++ src/main/java/edu/ufl/cise/p2p/message/MessageProcessor.java
@@ -8,19 +8,43 @@ import edu.ufl.cise.p2p.RemotePeer;
 public class MessageProcessor {
 
 	FileHandler fileHandler;
-	
-	
-	public MessageProcessor(FileHandler fileHandler){
+
+	public MessageProcessor(FileHandler fileHandler) {
 		this.fileHandler = fileHandler;
 	}
-	
-	public Message createResponse(Handshake handshake){
+
+	public Message createResponse(Handshake handshake) {
 		BitSet bitSet = fileHandler.getBitSet();
-		if(bitSet.isEmpty()) return null;
+		if (bitSet.isEmpty())
+			return null;
 		return new Bitfield(bitSet);
 	}
-	
+
 	public Message createResponse(Message message, RemotePeer rPeer) {
+		int type = message.getType();
+		switch (type) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			Bitfield bitfield = (Bitfield) message;
+			System.out.println("BitSet of size :"
+					+ bitfield.getBitSet().length() + " received from peer"
+					+ rPeer.getPeerId());
+			rPeer.setBitSet(bitfield.getBitSet());
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		}
 		return null;
 	}
 
