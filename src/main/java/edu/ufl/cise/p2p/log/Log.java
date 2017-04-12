b0d8936d9e69bd591c1216403f9738a56d443920
aniru <aniru@Rege-Dada>
2017-04-11 22:00:55 -0400

    Log changes

--- src/main/java/edu/ufl/cise/p2p/log/Log.java
+++ src/main/java/edu/ufl/cise/p2p/log/Log.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Date;
 
-/*
- * Log will be used by the torrent program to write events to a file. 
- */
+/**
+ * @author Anirudh Rege 
+ **/
 public class Log 
 {
 	/******************* Class Attributes *******************/
@@ -27,64 +27,56 @@ public class Log
 		Date date = new Date();
 		this.time = new Timestamp(date.getTime());
 		
-		/* Create the file and overwrite existing file */
 		file = new File(path);
 		//if(file.exists()) file.append();
 		file.createNewFile();
-		
-		/* Create the streams */
+
 		try 
 		{
 			fw = new FileWriter(file.getAbsoluteFile(), true);
 			bw = new BufferedWriter(fw);
-		} /* remove try */
+		} 
+
 		catch (IOException e) 
 		{
-			// TODO nothing?
-		} /* end catch */
+
+		} 
 		
-	} /* end constructor */
+	}
 	
-	/**
-	 * Thread safe method that allows the Log to write to the log.
-	 * @param content	the content being written to the log
-	 */
 	public final synchronized void writeToFile(String content)
 	{
 		try 
 		{
 			bw.write(content);
 			bw.flush();
-		} /* end try */ 
+		}
 		catch (IOException e) 
 		{
-			// TODO Do nothing?
-		} /* end catch */
+
+		} 
 		
-	} /* end writeToFile */
-	
-	/**
-	 * Closes the streams used to write to the file.
-	 */
+	}
+
 	public final synchronized void close()
 	{
 		try 
 		{
 			bw.close();
 			fw.close();
-		} /* end try */ 
+		}
 		catch (IOException e) 
 		{
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		} /* end catch */
+		}
 		
-	} /* end close method */
+	}
 	
 	public final synchronized String getTime()
 	{
 		Date date = new Date();
 		this.time.setTime(date.getTime());
 		return time.toString();
-	} /* end getTime method */	
-} /* end Log class */
+	}
+}
